de829c1fec1b9f605b92d12ee206d39a50e9f29b
Matthew Eric Bassett <mebassett@gegn.net>
2024-06-20 15:44:49 +0100

    adding an automerge repo.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Repo } from "@automerge/automerge-repo"
+import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
+import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb"
+
+const repo = new Repo({ network: [new BrowserWebSocketClientAdapter("ws://localhost:3030")]
+                      , storage: new IndexedDBStorageAdapter("qawolf-multiplayer-pad")
+                      })
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
