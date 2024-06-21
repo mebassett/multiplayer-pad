fa11fdf68822d6a19b283ae3cd0d7b897bd109b2
Matthew Eric Bassett <mebassett@gegn.net>
2024-06-21 14:23:51 +0100

    useLocalAwareness to track cursor position.
    Also useRemoteAwareness to track peers.
    
    Trying to reset cursor position after remote update, my best idea is to
    use the DocHandle to listen for changes, and record the lastUserEdited
    in the doc itself.  If the lastUserEdited doesn't match the current
    user, then update cursor position, otherwise leave it.  Seems like
    something that should be left into the emphemeral storage in the
    DocHandler itself (and exposed with local awareness / remote awareness).
    
    Changing the cursor position at every update means updating it locally
    moves the cursor around to the last position, breaking the editor.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,72 @@
 import './App.css'
-import Editor, { monaco } from '@monaco-editor/react'
-import {Text, next as Automerge } from "@automerge/automerge"
+import Editor from '@monaco-editor/react'
+import { type editor } from 'monaco-editor';
 import { AutomergeUrl } from '@automerge/automerge-repo'
-import { useDocument } from '@automerge/automerge-repo-react-hooks'
-import { MyDoc } from "./types.ts"
+import { useRepo, useHandle, useLocalAwareness, useRemoteAwareness, useDocument } from '@automerge/automerge-repo-react-hooks'
+import { MyDoc, EditorState } from "./types.ts"
 import { next as A } from "@automerge/automerge"
+import {useRef} from 'react'
 
 
-function App({ url } : { url: AutomergeUrl} ) {
+function App({ userId, url } : { userId: string, url: AutomergeUrl} ) {
   const [doc, changeDoc] = useDocument<MyDoc>(url) 
 
-  function onMonacoChange(newValue: string, e: monaco.IModelContentChangedEvent): void {
+  const repo = useRepo()
+
+  const handle = useHandle<EditorState>(url) || repo.create()
+
+  const editorRef = useRef(null as null | editor.IStandaloneCodeEditor)
+
+  const [localState, updateLocalState] = useLocalAwareness(
+      { handle
+      , userId
+      , initialState: { cursorPosition: {lineNumber: 1, column:1 }}
+      })
+
+  console.log(localState)
+
+
+  const [peerStates, _] = useRemoteAwareness({ handle, localUserId: userId})
+
+  function handleEditorDidMount(_editor : editor.IStandaloneCodeEditor) {
+      editorRef.current = _editor
+      _editor.onKeyDown(() => {
+          updateLocalState((s: EditorState) => ({...s, cursorPosition: _editor.getPosition() }))
+      })
+      _editor.onMouseDown(() => {
+          updateLocalState((s: EditorState) => ({...s, cursorPosition: _editor.getPosition() }))
+      })
+  }
+
+
+  function onMonacoChange(newValue: string | undefined) : void { //, e: Monaco.IModelContentChangedEvent): void {
 
 
     changeDoc((d: MyDoc) => {
-        A.updateText(d, ["text"], newValue)
-    })         
-    //console.log(e.changes)
+        A.updateText(d, ["text"], newValue || "")
+    })
   }
+
+  handle.update( d => {
+    if(editorRef.current) {
+      // editorRef.current.setPosition(localState.cursorPosition)
+      console.log("set cursor position?", localState.cursorPosition)
+    }
+    return d;
+  })
 
   return (
     <>
         <h1>Hello, world!</h1>
 
         <p> 
-          Count: <strong>{doc?.count ?? 0 }</strong>
+          Num peers: <strong>{Object.keys(peerStates).length ?? 0 }</strong>, Last Cursor Position: (line <strong>{localState.cursorPosition.lineNumber}</strong>, column: <strong>{localState.cursorPosition.column}</strong>)
         </p>
         <Editor height="90vh"
                 width="80vw" 
                 onChange={onMonacoChange}
                 value={doc?.text || "// multiplayer pad"}
+                onMount={handleEditorDidMount}
                 defaultLanguage="javascript" 
 
                 defaultValue="// multiplayer pad" />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from 'vite-plugin-wasm'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wasm(),react()],
+  plugins: [wasm(),react(), checker({ typescript: true})],
   worker: { format: "es", plugins: () => [wasm()], },
+  server: { host: true }
 })
