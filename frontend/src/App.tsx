7a70dff6d38bdaf710e61c45563b146e43af6416
Matthew Eric Bassett <mebassett@gegn.net>
2024-06-23 21:58:44 +0100

    using DocHandle's events rather than update func.
    I think I misread the code initially, and the events are a better way to
    do this.  update was for something else.
    
    I could use the `ephemeral-message` event to update peerStates manually
    instead of the useRemoteAwareness hook, but I'm running out of time to
    wrap this up.  So we'll leave it for now.
    
    Performance is much better when editing large files, except that it did
    crash chomium when I pasted in the large file from the firefox client.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Editor from '@monaco-editor/react'
 import { Range, type editor } from 'monaco-editor';
-import { AutomergeUrl } from '@automerge/automerge-repo'
+import { DocHandleChangePayload, AutomergeUrl } from '@automerge/automerge-repo'
 import { useRepo, useHandle, useLocalAwareness, useRemoteAwareness } from '@automerge/automerge-repo-react-hooks'
 import { emptyText, MyDoc, EditorState } from "./types.ts"
 import { next as A } from "@automerge/automerge"
@@ -59,61 +59,57 @@ function App({ userId, url } : { userId: string, url: AutomergeUrl} ) {
     })
   }
 
-  handle.update( newDoc => {
-    if(!docRef.current){
-        docRef.current = newDoc
-        return newDoc
-    }
-    if(editorRef.current) {
+  handle.on('ephemeral-message', () => {
+    if(decorationsRef.current)
+      decorationsRef.current.clear()
+    if(!editorRef.current) return
 
-      const doc = A.clone(docRef.current)
-
-      const binaryChanges = A.getChanges(doc, newDoc)
-      A.applyChanges(doc, binaryChanges, { patchCallback: (patches) => {
-
-          const edits = patches.map( (patch:any): editor.IIdentifiedSingleEditOperation | null => {
-            if(patch.action === 'splice') {
-              const { lineNumber: line, column: col } = editorRef.current!.getModel()!.getPositionAt(patch.path[1])
-              return { range: new Range(line, col, line, col)
-                     , text: patch.value
-                     , forceMoveMarkers: true
-                     }
-            }else if (patch.action === 'del') {
-              const { lineNumber: sLine, column: sCol } = editorRef.current!.getModel()!.getPositionAt(patch.path[1])
-              const { lineNumber: eLine, column: eCol } = editorRef.current!.getModel()!.getPositionAt(patch.path[1] + (patch.length || 1))
-              return { range: new Range(sLine, sCol, eLine, eCol)
-                     , text: null
-                     , forceMoveMarkers: true
-                     }
-            } else return null
-          }).filter ( (a) : a is editor.IIdentifiedSingleEditOperation => !!a)
-          if(newDoc.text !== doc.text && newDoc.text !== editorRef.current!.getModel()!.getValue())
-            editorRef.current!.executeEdits('automerge', edits)
-
-      }})  
-      
-
-      if(decorationsRef.current)
-        decorationsRef.current.clear()
-
-      const decorations =
-        Object.keys(peerStates).map( (uid: string): editor.IModelDeltaDecoration => ({
-           range: new Range( peerStates[uid].cursorPosition.lineNumber
-                           , peerStates[uid].cursorPosition.column
-                           , peerStates[uid].cursorPosition.lineNumber
-                           , peerStates[uid].cursorPosition.column)
-         , options: { hoverMessage: { value: uid }
-                    , className: "fakeCursor"
-                    }
-         }))
-      decorationsRef.current = editorRef.current.createDecorationsCollection(decorations)
-
-    }
-
-    docRef.current = newDoc
-    return newDoc
+    const decorations =
+    // we could juse inspect the event rather than useRemoteAwareness but nah..
+    // this does introduce a subtle bug where the cursor lags one step behind
+    // but I'm running out of time to fix stuff like this.
+      Object.keys(peerStates).map( (uid: string): editor.IModelDeltaDecoration => ({
+         range: new Range( peerStates[uid].cursorPosition.lineNumber
+                         , peerStates[uid].cursorPosition.column
+                         , peerStates[uid].cursorPosition.lineNumber
+                         , peerStates[uid].cursorPosition.column)
+       , options: { hoverMessage: { value: uid }
+                  , className: "fakeCursor"
+                  }
+       }))
+    decorationsRef.current = editorRef.current.createDecorationsCollection(decorations)
   })
 
+  handle.on('change', (e: DocHandleChangePayload<MyDoc>):void => {
+    if(!docRef.current){
+        docRef.current = e.doc
+        return
+    }
+    if(editorRef.current) {
+      const doc = e.patchInfo.before
+      const newDoc = e.patchInfo.after
+      if(newDoc.text !== doc.text && newDoc.text !== editorRef.current!.getModel()!.getValue()) {
+        const edits = e.patches.map( (patch:any): editor.IIdentifiedSingleEditOperation | null => {
+          if(patch.action === 'splice') {
+            const { lineNumber: line, column: col } = editorRef.current!.getModel()!.getPositionAt(patch.path[1])
+            return { range: new Range(line, col, line, col)
+                   , text: patch.value
+                   , forceMoveMarkers: true
+                   }
+          }else if (patch.action === 'del') {
+            const { lineNumber: sLine, column: sCol } = editorRef.current!.getModel()!.getPositionAt(patch.path[1])
+            const { lineNumber: eLine, column: eCol } = editorRef.current!.getModel()!.getPositionAt(patch.path[1] + (patch.length || 1))
+            return { range: new Range(sLine, sCol, eLine, eCol)
+                   , text: null
+                   , forceMoveMarkers: true
+                   }
+          } else return null
+        }).filter ( (a) : a is editor.IIdentifiedSingleEditOperation => !!a)
+        editorRef.current!.executeEdits('automerge', edits)
+      }
+      docRef.current = newDoc
+    }
+  })
 
 
   return (
